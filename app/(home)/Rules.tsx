7187bfc2b1e8d9e929a8c8991ff701ce2f7d99b8
Sebastian <me@wondering.website>
2026-09-04 18:03:45 -0400

    1. add human/ai division split 2. update rules to reflect internal ctf restrictions

--- app/(home)/Rules.tsx
+++ app/(home)/Rules.tsx
@@ -9,17 +9,24 @@ export default function Rules() {
             </SectionHeader>
             <ol className="list-decimal list-outside pl-6 space-y-2">
                 <li>
-                    During the competition, each person may only be a part of one team total, and only members of a
-                    given team may assist in solving a challenge for that team.
+                    Teams are limited to one person per account.
+                </li>
+                <li>
+                    You are welcome to ask for/give help with challenges, but don't share flags or submit flags from online.
+                </li>
+                <li>
+                    Teams in the "Purdue (human)" division are not permitted to use AI to solve challenges.
+                    Reading the automatic AI summary from a search or asking an LLM for generic advice is fine;
+                     providing an LLM with exact challenge details is not.
+                </li>
+                <li>
+                    Teams in the "Purdue (unrestricted)" division can use AI to solve challenges; however,
+                    please try to at least understand the solution before submitting the flag.
                 </li>
                 <li>
                     Flags are of the format{' '}
                     <code className="bg-black/40 px-2 py-1 text-primary rounded">{'bctf{[ -~]+}'}</code>{' '}
                     unless otherwise noted on the challenge description. No brute-force guessing flags.
-                </li>
-                <li>
-                    No flag or hint sharing. Do not solicit or accept hints or guidance from any person except through
-                    official support channels.
                 </li>
                 <li>
                     Do not attempt to attack or interfere with other teams or any servers used in this competition that
